6bf4598dfed02d47a47e06fdd632f55b80af2861
JuanDeLeon24 <124088184+JuanDeLeon24@users.noreply.github.com>
2023-02-25 19:13:26 -0500

    Adicion de inputs

---
+++ src/pages/Login/components/input/Input.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Input.css'
+
+const Input = () => {
+    return (
+        <div>
+            <input 
+            
+            />
+        </div>
+    )
+};
+
+export default Input;
