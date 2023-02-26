22451d4c6767b19b51165b22d797df56fdebcdb5
JuanDeLeon24 <124088184+JuanDeLeon24@users.noreply.github.com>
2023-02-25 19:52:13 -0500

    Login basico

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,17 +1,58 @@
-import React from "react";
+import React, {useState} from "react";
 import './Login.css'
 import Title from './components/Title/Title'
 import Label from "./components/Label/Label";
 import Input from "./components/Input/Input";
 
 const Login = () => {
+
+    const [ user, setUser ] = useState('');
+    const [ password, setPassword ] = useState('');
+
+    function handleChange(name, value){
+        if(name === 'Usuario'){
+            setUser(value)
+        }else {
+            setPassword(value)
+        }
+    };
+
+    function handleSubmit(){
+        let account = {user, password}
+        if(account){
+            console.log('account: ', account)
+        }
+    };
+
     return(
         <div className='login-container'>
-            <Title text='No soy un titulo'/>
+            <Title text='Soy un titulo'/>
+
             <Label text='Usuario'/>
-            <Input />
+            <Input 
+            attribute={{
+                id: 'usuario',
+                name: 'usuario',
+                type: 'text',
+                placeholder: 'Ingrese su usuario'
+            }}
+            handleChange={handleChange}
+            />
+
             <Label text='Contraseña'/> 
-            <Input />
+            <Input 
+            attribute={{
+                id: 'contraseña',
+                name: 'contraseña',
+                type: 'password',
+                placeholder: 'Ingrese su contraseña'
+            }}
+            handleChange={handleChange}
+            />
+            <button onClick={handleSubmit}>
+                    Login
+            </button>
+
         </div>
     )
 };
--- src/pages/Login/components/input/Input.jsx
+++ src/pages/Login/components/input/Input.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import './Input.css'
 
-const Input = () => {
+const Input = ({ attribute, handleChange, param}) => {
     return (
         <div>
             <input 
-            
+            id={attribute.id}
+            name={attribute.name}
+            placeholder={attribute.placeholder}
+            type={attribute.type}
+            onChange={ (e) => handleChange(e.target.name, e.target.value)}
+            className='regular-style'
             />
         </div>
     )
